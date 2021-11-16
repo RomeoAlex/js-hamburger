3362fc89c8f12332158fc95e62ad584eb93ad7ba
RomeoAlex <romeo.alessandro17@gmail.com>
2021-11-16 16:45:35 +0100

    non funziona

--- main.js
+++ main.js
@@ -1,42 +1,44 @@
 //la classe del burger non apre il burger ma lo apre la <a> padre perciò devo selezionarlo
-const openMenu = document.querySelector("div.header-right i");
+// const openMenu = document.querySelector("div.header-right i");
 
-// seleziono l'elemento corretto usato console per comprendere l'elemento selezionato
-console.log(openMenu);
+// // seleziono l'elemento corretto usato console per comprendere l'elemento selezionato
+// console.log(openMenu);
 
 // seleziono la classe del burger per utilizzarla in seguito
-const openBurger = document.querySelector("div.hamburger-menu");
-console.log(openBurger);
-//aggiungo evento sul click
-openMenu.addEventListener('click',
-    function(){
-        openBurger.style.display = "block";
+// const openBurger = document.querySelector("div.hamburger-menu");
+// console.log(openBurger);
+// //aggiungo evento sul click
+// openMenu.addEventListener('click',
+//     function(){
+//         openBurger.style.display = "block";
+//     }
+//     );
+// // adesso seleziono l'elemento di chiusura del menu
+// const closeMenu = document.querySelector("a.close i");
+// // uso console per comprendere se ho selezionato il corretto elemento nel DOM
+// console.log(closeMenu);
+
+// //aggiungo evento sul click
+// closeMenu.addEventListener('click',
+//     function(){
+//         openBurger.style.display = "none";
+//     }
+//     );
+
+    // Il professore utilizza add e remove
+    const burgerButton = document.querySelector('.header-right > a');
+    console.log(burgerButton);
+    burgerButton.addEventListener('click',
+    function() {
+        const hamburgerMenu = document.querySelector('.hamburger-menu');
+        console.log(hamburgerMenu);
+        hamburgerMenu.classList.add('active')
     }
     );
-// adesso seleziono l'elemento di chiusura del menu
-const closeMenu = document.querySelector("a.close i");
-// uso console per comprendere se ho selezionato il corretto elemento nel DOM
-console.log(closeMenu);
-
-//aggiungo evento sul click
-closeMenu.addEventListener('click',
-    function(){
-        openBurger.style.display = "none";
+    const closeButton = document.querySelector('.close');
+    burgerButton.addEventListener('click',
+    function() {
+        const hamburgerMenu = document.querySelector('.hamburger-menu');
+        hamburgerMenu.classList.remove('active')
     }
     );
-
-    //Il professore utilizza add e remove
-    // const burgerButton = document.querySelector('.header-right > a');
-    //burgerButton.addEventListener('click',
-    // function(){
-    //     const hamburgerMenu = document.querySelector('.hamburger-menu');
-    //     hamburgerMenu.classList.add('active')
-    // }
-    // );
-    //const closeButton = document.querySelector('.close');
-    //burgerButton.addEventListener('click',
-    // function(){
-    //     const hamburgerMenu = document.querySelector('.hamburger-menu');
-    //     hamburgerMenu.classList.remove('active')
-    // }
-    // );
